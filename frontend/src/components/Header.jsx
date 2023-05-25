06b28c03f28efa901f30d3ecdef078ad97621cc1
z.tokarczyk <z.tokarczyk.626@studms.ug.edu.pl>
2023-05-25 09:28:06 +0200

    add a header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-medium">
+      <div>Header</div>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 function Home() {
   return (
-    <div>
+    <div className="flex flex-col">
+      <Header />
       <h1 className="underline"> Home</h1>
     </div>
   );
