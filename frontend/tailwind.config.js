3eeed75eaf829f6dfd5e10f2db21e7a7734265df
z.tokarczyk <z.tokarczyk.626@studms.ug.edu.pl>
2023-05-25 09:27:50 +0200

    add theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      dark: "#132A13",
+      medium: "#31572C",
+      normal: "#4F772D",
+      light: "#90A955",
+      yellow: "#ECF39E",
+    },
     extend: {},
   },
   plugins: [],
